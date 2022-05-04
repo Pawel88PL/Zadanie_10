6f960fc9b180679c3db9be9fa27e1f6fa76ecc3a
Pawel Staniul <pstaniul@gmail.com>
2022-05-04 18:51:12 +0200

    Działający program

--- Program.cs
+++ Program.cs
@@ -15,50 +15,54 @@ namespace Zadanie_10
         static void Main(string[] args)
         {
             string koniec;
-            int wynik;
+            int wynik, stopien, wyraz, x;
+            
 
-            Console.WriteLine("Program oblicza wartość wielomianu.");
+            Console.WriteLine("Program oblicza wartość wielomianu do max 3 stopnia.");
+            Console.WriteLine();
+
             do
             {
                 Console.Write("Podaj stopień wielomianu: ");
-                if (int.TryParse(Console.ReadLine(), out int stopien))
+                while (!(int.TryParse(Console.ReadLine(), out stopien) && stopien > 0))
                 {
-                    
-                    Console.Write("Podaj wyraz wolny: ");
-                    if (int.TryParse(Console.ReadLine(), out int wyraz))
-                    {
-                        int [] tablica = new int [stopien];
-                        for (int i = 0; i < stopien; i++)
-                        {
-                            Console.Write("Podaj współczynnik przy x^" + (i + 1) + ": ");
-                            int.TryParse(Console.ReadLine(), out tablica[i]);
-                            
-                        }
-                        Console.Write("Podaj wartość x: ");
-                        if (int.TryParse(Console.ReadLine(), out int x))
-                        {
-                            wynik = (tablica[0] * (x * 1)) + (tablica[1] * (x * x)) + (tablica[2] * (x * x * x)) + wyraz;
-                            Console.WriteLine("Wartość wielomianu wynosi: {0}", wynik);
-                            Console.WriteLine("Czy chcesz zakończyć program? Tak - Enter, Nie - wpisz słowo 'nie'.");
-                            koniec = Console.ReadLine();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Błędna wartość");
-                            koniec = "nie";
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Błędna wartość.");
-                        koniec = "nie";
-                    }
+                    Console.WriteLine("Będna wartość.");
+                    Console.Write("Podaj stopień wielomianu: ");
                 }
-                else
+                
+                Console.Write("Podaj wyraz wolny: ");
+                while (!(int.TryParse(Console.ReadLine(), out wyraz) && wyraz != 0))
                 {
                     Console.WriteLine("Błędna wartość.");
-                    koniec = "nie";
+                    Console.Write("Podaj wyraz wolny: ");
                 }
+                
+                int[] tablica = new int[stopien];
+                
+                for (int i = 0; i < stopien; i++)
+                {
+                    Console.Write("Podaj współczynnik przy x^" + (i + 1) + ": ");
+                    while (!(int.TryParse(Console.ReadLine(), out tablica[i])))
+                    {
+                        Console.WriteLine("Nieprawidłowa wartość");
+                        Console.Write("Podaj współczynnik przy x^ " + (i + 1) + ": ");
+                    }
+                }
+                
+                Console.Write("Podaj wartość x: ");
+                while(!(int.TryParse(Console.ReadLine(), out x) && x != 0))
+                {
+                    Console.WriteLine("Błędna wartość.");
+                    Console.Write("Podaj wartość x: ");
+                }
+                
+                wynik = (tablica[0] * (x * 1)) + (tablica[1] * (x * x)) + (tablica[2] * (x * x * x)) + wyraz;
+                
+                Console.WriteLine("Wartość wielomianu wynosi: {0}", wynik);
+                Console.WriteLine();
+                
+                Console.WriteLine("Czy chcesz zakończyć program? Tak - Enter, Nie - wpisz słowo 'nie'.");
+                koniec = Console.ReadLine();
             }
             while (koniec == "nie");
         }
